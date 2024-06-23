bc0f1c4f196bc688bd7c7f1c8a38ae7c10c98afd
Kamal Shafi <muha.kamal@gmail.com>
2024-06-23 16:55:48 +0800

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from "express";
-import process from "process";
+import process, { env } from "process";
 import weddingRoutes from "./routes/wedding";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import cors from "cors";
+
+const origin: { [key: string]: string[] } = {
+  dev: ["http://localhost:4200"],
+  // TODO: add prod url
+  prod: [""],
+};
+const environment: string = process.env.ENVIRONMENT || "prod";
+var corsOptions = {
+  origin: origin[environment],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use("/wedding", weddingRoutes);
+app.use("/wedding", cors(corsOptions), weddingRoutes);
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
